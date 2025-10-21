99b4eff1219fb6d03f8e0f8182baccf444037556
AndrewMecDev <antonyalca.pe@gmail.com>
2025-10-21 14:19:05 -0500

    Creacion y adicion de los archivos SerieApiService-SerieModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,12 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
